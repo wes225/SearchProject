a15da768f344f40ac005c992c14f92e5a95d32a3
Ouissem Hammami <ouissem.hammami@desjardins.com>
2018-08-20 15:59:41 -0400

    Adds structure to search results

---
+++ src/Components/AdvisorResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdvisorResults extends Component {
+  render() {
+    return (
+      <div>
+        Advisor Resultssss
+      </div>
+    )
+  }
+}
--- src/Components/SearchMenu.js
+++ src/Components/SearchMenu.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import TeamResults from '../Components/TeamResults'
+import AdvisorResults from '../Components/AdvisorResults'
 
 export default class SearchMenu extends Component {
     constructor(props) {
@@ -11,23 +13,39 @@ export default class SearchMenu extends Component {
         }
     }
     render() {
+        let header = this.state.teamSelected? <TeamResults/> : <AdvisorResults/>
         return (
             <div>
                 <h2>Trouver un conseiller</h2>
                 <div className="searchType">
                     <ul>
-                        <input type="checkbox" name="Par Equipe" id="searchByTeam" />
-                        <input type="checkbox" name="Par conseiller" id="searchByAdvisor" /></ul>
+                    <label>
+              Par équipe
+              <input type="radio" name="team" value="team" id="searchByTeam" />
+            </label>
+            <label>
+              Par conseiller
+              <input
+                type="radio"
+                name="advisor"
+                value="advisor"
+                id="searchByAdvisor"
+              />
+            </label></ul>
                 </div>
                 <div className="searchCriteria">
                     <ul>
                         <input type="text" name="Name" id="" />
                         <select name="Région" id=""></select>
-                        <select name="Expertuse" id=""></select>
-                        <button></button>
+                        <select name="Expertise" id=""></select>
+                        <button>Reinitialiser</button>
                     </ul>
                 </div>
+                <div>
+                    {header}
+                </div>
             </div>
+
         )
     }
 }
