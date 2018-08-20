7b8da1e22de51702a4463923d644c21cb67a39a1
Ouissem Hammami <ouissem.hammami@desjardins.com>
2018-08-20 15:19:23 -0400

    Imports SearchMenu

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+import SearchMenu from "./Components/SearchMenu";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <SearchMenu></SearchMenu>
     </div>
   );
 }
