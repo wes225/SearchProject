a0565774c95defc11d06697d9136fa784eb45ffd
Ouissem Hammami <ouissem.hammami@desjardins.com>
2018-08-21 09:28:52 -0400

    Adds conditional rendering for radio button change

--- src/Components/SearchMenu.js
+++ src/Components/SearchMenu.js
@@ -6,14 +6,21 @@ export default class SearchMenu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            teamSelected: false,
+            advisorSelected: false,
             searchBarContent: "",
             myData: props
 
         }
+        this.handleRadioChange = this.handleRadioChange.bind(this);
+    }
+    handleRadioChange(event) {
+        if (event.target.value == "team") {
+            this.setState({ advisorSelected: false })
+        } else { this.setState({ advisorSelected: true }) }
+        console.log(event.target.value);
     }
     render() {
-        let header = this.state.teamSelected? <TeamResults/> : <AdvisorResults/>
+        const Header = () => this.state.advisorSelected ? <AdvisorResults /> : <TeamResults />;
         return (
             <div>
                 <h2>Trouver un conseiller</h2>
@@ -21,35 +28,43 @@ export default class SearchMenu extends Component {
                     <form>
 
 
-                    <label>
-              Par équipe
-              <input type="radio" name="searchBy" value="team" id="searchByTeam" checked={true} />
-            </label>
-            <label>
-              Par conseiller
+                        <label>
+                            Par équipe
               <input
-                type="radio"
-                name="searchBy"
-                value="advisor"
-                id="searchByAdvisor"
-              />
-            </label>
-              </form>  </div>
+                                type="radio"
+                                name="searchBy"
+                                value="team"
+                                id="searchByTeam"
+                                checked={!this.state.advisorSelected}
+                                onChange={this.handleRadioChange} />
+                        </label>
+                        <label>
+                            Par conseiller
+              <input
+                                type="radio"
+                                name="searchBy"
+                                value="advisor"
+                                id="searchByAdvisor"
+                                onChange={this.handleRadioChange}
+                                checked={this.state.advisorSelected}
+                            />
+                        </label>
+                    </form>  </div>
                 <div className="searchCriteria">
                     <ul>
                         <input type="text" name="Name" id="" />
                         <form>
-                        <select name="Région" id=""></select>
-                        <select name="Expertise" id=""></select>
+                            <select name="Région" id=""></select>
+                            <select name="Expertise" id=""></select>
                         </form>
                         <button>Reinitialiser</button>
                     </ul>
                 </div>
-                <div>
-                    {header}
-                </div>
+                <Header />
             </div>
 
         )
     }
 }
+
+
